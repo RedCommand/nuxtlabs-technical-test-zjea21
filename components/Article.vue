d52431216d72a845849602a7b5f2d3c919bd4c9b
RedCommand <maxvidal1612@gmail.com>
2022-06-09 01:55:44 +0200

    [ADD] BONNUSES !
    [ADD] button to reverse sort order
    
    [ADD] automatics filters for tags
    
    [ADD] search bar to search titles

--- components/Article.vue
+++ components/Article.vue
@@ -1,11 +1,16 @@
-
 <template>
     <div class="pt-8">
         <div class="flex items-center gap-3">
             <!-- TODO: Display article tags -->
             <span
                 v-for="tag in article.tags"
-                class="px-2 py-0.5 rounded-full text-xs text-sky-900 font-medium bg-sky-200"
+                :key="tag"
+                class="px-2 py-0.5 rounded-full text-xs font-medium"
+                :class="
+                    selectedTags.length == 0 || selectedTags.indexOf(tag) !== -1
+                        ? 'text-sky-900 bg-sky-200'
+                        : 'text-gray-900 bg-gray-200'
+                "
                 >{{ tag }}</span
             >
         </div>
@@ -14,12 +19,12 @@
                 {{ article.title }}
             </p>
             <p class="mt-1 text-base text-gray-500">
-				{{ article.description }}
+                {{ article.description }}
             </p>
         </a>
         <div v-if="article.author" class="mt-6 flex items-center">
             <div class="flex-shrink-0">
-				<a :href="article.author.link" target="_blank">
+                <a :href="article.author.link" target="_blank">
                     <span class="sr-only">{{ article.author.name }}</span>
                     <img
                         class="h-10 w-10 rounded-full"
@@ -29,13 +34,13 @@
                 </a>
             </div>
             <div class="ml-3">
-				<p class="text-sm font-medium text-gray-900">
-					<a :href="article.author.link" target="_blank">
+                <p class="text-sm font-medium text-gray-900">
+                    <a :href="article.author.link" target="_blank">
                         {{ article.author.name }}
                     </a>
                 </p>
                 <p class="flex space-x-1 text-sm text-gray-500">
-					<!-- TODO: Display formatted date -->
+                    <!-- TODO: Display formatted date -->
                     <a>{{ formatDate(article.date) }}</a>
                 </p>
             </div>
@@ -46,14 +51,17 @@
 <script setup>
 // TODO: Get `article` from props
 // https://vuejs.org/api/sfc-script-setup.html#defineprops-defineemits
-import { formatDate } from '../utils/index.ts'
+import { formatDate } from "../utils/index.ts";
 
 const props = defineProps({
-	article: Object,
+    article: Object,
+    selectedTags: Array,
 });
 
+const selectedTags = ref(props.selectedTags);
+
 const article = ref({
-	_path: props.article._path,
+    _path: props.article._path,
     title: props.article.title,
     description: props.article.description,
     author: props.article.author,
--- pages/index.vue
+++ pages/index.vue
@@ -7,18 +7,84 @@
             <p class="mt-3 text-lg text-gray-400 mt-2">
                 Discover articles from Nuxt team.
             </p>
+            <div class="text-right">
+			<input type="text" v-model="search" placeholder="Search titles..." />
+                <button @click="sortDirection = !sortDirection">
+                    <p v-if="sortDirection">Date ↑</p>
+                    <p v-else>Date ↓</p>
+                </button>
+            </div>
         </div>
-
+        <div class="text-center grid grid-cols-5 gap-4">
+            <ul v-for="tag in tags" :key="tag">
+                <button
+                    @click="filterTag(tag)"
+                    class="px-2 py-0.5 rounded-full text-xs font-medium"
+                    :class="
+                        selectedTags.length == 0 ||
+                        selectedTags.indexOf(tag) !== -1
+                            ? 'text-sky-900 bg-sky-200'
+                            : 'text-gray-900 bg-gray-200'
+                    "
+                >
+                    {{ tag }}
+                </button>
+            </ul>
+        </div>
         <!-- TODO: List of `Article` -->
         <div class="flex flex-col gap-8 divide-y divide-gray-100">
-            <ul v-for="article in articles">
-                <Article :article="article" />
+            <ul
+                v-for="article in sortDirection
+                    ? matchTags(articles, selectedTags, search)
+                    : articles.slice().reverse()"
+                :key="article"
+            >
+                <Article :article="article" :selectedTags="selectedTags" />
             </ul>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+const sortDirection = ref(true);
+const selectedTags = ref([]);
+const search = ref("");
+
+function filterTag(tag) {
+    const index = selectedTags.value.indexOf(tag);
+
+    if (index !== -1) {
+        selectedTags.value.splice(index, 1);
+    } else {
+        selectedTags.value.push(tag);
+    }
+}
+
+function matchTags(articles, selectedTags, search) {
+    if (selectedTags.length === 0 && search.length === 0) {
+        return articles;
+    }
+    const matches = [];
+
+    if (selectedTags.length === 0) {
+        matches.push(...articles);
+    } else {
+        articles.forEach((article) => {
+            if (article.tags.some((v) => selectedTags.indexOf(v) !== -1)) {
+                matches.push(article);
+            }
+        });
+    }
+    if (search.length === 0) {
+        return matches;
+    }
+    return matches.filter((article) =>
+        article.title.toLowerCase().includes(search.toLowerCase())
+    );
+}
+
 const { data: articles } = await useAsyncData("articles", () => {
     // TODO: Fetch articles from `content/` ordered by date DESC
     // https://content.nuxtjs.org/api/composables/query-content#find
@@ -28,5 +94,21 @@ const { data: articles } = await useAsyncData("articles", () => {
             .find()
             .then((articles) => resolve(articles));
     });
+});
+
+const { data: tags } = await useAsyncData("tags", async () => {
+    // TODO: Fetch articles from `content/` ordered by date DESC
+    // https://content.nuxtjs.org/api/composables/query-content#find
+    const tags = [];
+
+    await articles._rawValue.forEach(async (article) => {
+        await article.tags.forEach(async (tag) => {
+            if (!tags.includes(tag)) {
+                tags.push(tag);
+            }
+        });
+    });
+
+    return tags;
 });
 </script>
